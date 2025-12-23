87dcce35fe8e83ef93215d8f4f26294e6640f28c
Sandello <hrunun@bk.ru>
2025-12-24 03:49:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
-
+import asyncio
 import os
 
 # Токен бота из переменных окружения
